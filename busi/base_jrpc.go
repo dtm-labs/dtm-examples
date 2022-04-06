1df24e79096c0fffec333e87a3f24d5239ecb9db
yedf2 <120050102@qq.com>
2022-04-06 09:42:26 +0800

    add jrpc

---
+++ busi/base_jrpc.go
@@ -0,0 +1,38 @@
+package busi
+
+import (
+	"fmt"
+
+	"github.com/dtm-labs/dtmcli/dtmimp"
+	"github.com/dtm-labs/dtmcli/logger"
+	"github.com/dtm-labs/dtm-examples/dtmutil"
+	"github.com/gin-gonic/gin"
+)
+
+// BusiJrpcURL url prefix for busi
+var BusiJrpcURL = fmt.Sprintf("http://localhost:%d/api/json-rpc?method=", BusiPort)
+
+func addJrpcRoute(app *gin.Engine) {
+	app.POST("/api/json-rpc", dtmutil.WrapHandler2(func(c *gin.Context) interface{} {
+		var data map[string]interface{}
+		err := c.BindJSON(&data)
+		dtmimp.E2P(err)
+		logger.Debugf("method is: %s", data["method"])
+		var rerr map[string]interface{}
+		r := MainSwitch.JrpcResult.Fetch()
+		if r != "" {
+			rerr = map[string]interface{}{
+				"code": map[string]int{
+					"FAILURE": dtmimp.JrpcCodeFailure,
+					"ONGOING": dtmimp.JrpcCodeOngoing,
+					"OTHER":   -23977,
+				},
+			}
+		}
+		return map[string]interface{}{
+			"jsonrpc": "2.0",
+			"error":   rerr,
+			"id":      data["id"],
+		}
+	}))
+}
